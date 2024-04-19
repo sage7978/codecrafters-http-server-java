a0bc1f324d6f2a269b6a2d5684360e777390fe7f
sage7978 <44961824+sage7978@users.noreply.github.com>
2024-04-19 08:47:32 +0530

    parse-headers user agent task commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -33,30 +33,20 @@ public class Main {
            HttpRequest httpRequest = HttpRequestBuilder.parseFromInputStream(clientSocket.getInputStream());
            /*       HttpRequest System Print        */
            System.out.println(httpRequest.toString());
-//           String[] paths = httpRequest.getPath().split("/");
            HttpResponse response = null;
            String body = null;
            String path = httpRequest.getPath();
-           if(path.equals("/")){
+
+           if(path.equals("/user-agent")){
+               body = httpRequest.getHeaders().getOrDefault("User-Agent", "");
                response = new HttpResponseBuilder()
                        .version("HTTP/1.1")
                        .status("OK")
                        .statusCode(200)
                        .method("GET")
-                       .addHeader("Content-Length", String.valueOf(0))
-                       .addHeader("Content-Type", "text/plain")
-                       .build();
-           }
-           else if(path.startsWith("/echo")){
-               body = httpRequest.getPath().substring(6);
-               response = new HttpResponseBuilder()
-                       .version("HTTP/1.1")
-                       .status("OK")
-                       .statusCode(200)
-                       .method("GET")
-                       .body(body)
                        .addHeader("Content-Length", String.valueOf(body.length()))
                        .addHeader("Content-Type", "text/plain")
+                       .body(body)
                        .build();
            }
            else {
